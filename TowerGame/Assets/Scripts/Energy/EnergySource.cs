15ae5796ec3f7c7874404d9b95403e063749a9cc
27chandler <chandler27s@yahoo.com>
2022-03-19 17:21:55 +0000

    Added grid shaders and particle systems to energy sources

--- TowerGame/Assets/Scripts/Energy/EnergySource.cs
+++ TowerGame/Assets/Scripts/Energy/EnergySource.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class EnergySource : MonoBehaviour
 {
+    [SerializeField] protected ParticleSystem particles;
+    [SerializeField] protected bool usesParticles = false;
     [SerializeField] protected float energyValue;
     [SerializeField] [ReadOnly] protected float totalPower;
+    protected ParticleSystem.EmissionModule emmisionSystem;
 
     public float TotalPower { get => totalPower; set => totalPower = value; }
 
     protected virtual void Start()
     {
+        if (usesParticles)
+        {
+            emmisionSystem = particles.emission;
+
+            emmisionSystem.rateOverTimeMultiplier = totalPower * 10.0f;
+        }
+
         EnergySystem.AddSource(this);
     }
 
